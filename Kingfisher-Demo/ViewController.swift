946f25780fa3756f8d29c830ecaae224f72179d2
Kevin Taniguchi <ktaniguchi@urbn.com>
2016-06-12 13:46:42 -0400

    cleanup

--- Kingfisher-Demo/ViewController.swift
+++ Kingfisher-Demo/ViewController.swift
@@ -31,13 +31,13 @@ class ViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         title = "Kingfisher"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
     @IBAction func clearCache(sender: AnyObject) {
